925d623cc7e7243649dc9c44df9cb8e603e640d5
EgorDyakonov <egor.dyakonov666@gmail.com>
2020-03-29 23:04:08 +0300

    corect login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,30 +1,31 @@
-function registerLogin() {
-    var register = document.querySelector(".register_btn");
-    var login = document.querySelector(".login_btn");
-    var loginMainFrst = document.querySelectorAll(".login_main");
-    var welcome = document.querySelectorAll(".welcome");
+(() => {
+  const register = document.querySelector('.register_btn');
+  const login = document.querySelector('.login_btn');
+  const loginMainFrst = document.querySelectorAll('.login_main');
+  const welcome = document.querySelectorAll('.welcome');
+  const none = 'none';
+  const flex = 'flex';
 
-    loginMainFrst[1].style.display = "none";
+  loginMainFrst[1].style.display = none;
 
-    register.onclick = function () {
-        if (window.matchMedia("(max-width: 768px)").matches) {
-            register.style.display = "none";
-            login.style.display = "flex";
-        }
-        loginMainFrst[0].style.display = "none";
-        loginMainFrst[1].style.display = "flex";
-        welcome[1].style.paddingBottom = "4%";
-        document.querySelector(".input_login_click").style.marginBottom = "4.5%";
-        document.querySelector(".reg_btn").style.marginTop = "5.4%";
+  register.onclick = () => {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      register.style.display = none;
+      login.style.display = flex;
     }
+    loginMainFrst[0].style.display = none;
+    loginMainFrst[1].style.display = flex;
+    welcome[1].style.paddingBottom = '4%';
+    document.querySelector('.input_login_click').style.marginBottom = '4.5%';
+    document.querySelector('.reg_btn').style.marginTop = '5.4%';
+  }
 
-    login.onclick = function () {
-        if (window.matchMedia("(max-width: 768px)").matches) {
-            login.style.display = "none";
-            register.style.display = "flex";
-        }
-        loginMainFrst[0].style.display = "flex";
-        loginMainFrst[1].style.display = "none";
+  login.onclick = () => {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      login.style.display = none;
+      register.style.display = flex;
     }
-}
-registerLogin();
+    loginMainFrst[0].style.display = flex;
+    loginMainFrst[1].style.display = none;
+  }
+})();
